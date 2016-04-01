edf393f5f5c8a64433af1b3e3ee8b3269455991d
PratikPatel13 <PratikPatel13@missionrnd.com>
2016-04-01 15:24:20 +0530

    Submit Commit

--- src/BSTTransversals.cpp
+++ src/BSTTransversals.cpp
@@ -9,7 +9,7 @@ Note : Memory for the Array is already allocated ,Dont allocate it again using m
 ->Take help of helper functions which are actually solving the problem .
 ->Dont Process Invalid Inputs
 
-Bonus Task : 
+Bonus Task :
 ->Go to the BSTTransversals Spec File ,We have left a custom test case for you ,Try to fill
 it and understand how testing works .
 */
@@ -23,13 +23,60 @@ struct node{
 };
 
 
-void inorder(struct node *root, int *arr){
-	
+void inorder_helper(struct node *root, int *arr, int *i){
+	if (root)
+	{
+		inorder_helper(root->left, arr, i);
+		arr[*i] = root->data;
+		(*i)++;
+		inorder_helper(root->right, arr, i);
+	}
 }
-void preorder(struct node *root, int *arr){
-	
+
+void inorder(struct node *root, int *arr)
+{
+	int i = 0;
+	if (root && arr)
+	{
+		inorder_helper(root, arr, &i);
+	}
 }
-void postorder(struct node *root, int *arr){
-	
+void preorder_helper(struct node *root, int *arr, int *i){
+	if (root)
+	{
+		arr[*i] = root->data;
+		(*i)++;
+		preorder_helper(root->left, arr, i);
+		preorder_helper(root->right, arr, i);
+	}
+}
+
+void preorder(struct node *root, int *arr)
+{
+	int i = 0;
+	if (root && arr)
+	{
+		preorder_helper(root, arr, &i);
+	}
+}
+
+void postorder_helper(struct node *root, int *arr, int *i){
+	if (root)
+	{
+		postorder_helper(root->left, arr, i);
+		postorder_helper(root->right, arr, i);
+		arr[*i] = root->data;
+		(*i)++;
+
+	}
+}
+
+void postorder(struct node *root, int *arr)
+{
+	int i = 0;
+	if (root && arr)
+	{
+		postorder_helper(root, arr, &i);
+	}
 }
 
